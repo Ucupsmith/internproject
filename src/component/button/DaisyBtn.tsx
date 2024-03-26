fe334069b6c1cd9960a8ffbc516470d15561d7a5
ucupsmith <akilnauffancozz@gmail.com>
2024-03-26 14:36:35 +0700

    daisy button & input

--- src/component/button/DaisyBtn.tsx
+++ src/component/button/DaisyBtn.tsx
@@ -1,25 +1,14 @@
 import React from 'react'
-import { Button } from 'react-daisyui'
+
 const DaisyBtn = () => {
   return (
     <>
       <div className="flex flex-col justify-center items-center">
           <h1 className='font-bold text-4xl uppercase'>daisy button</h1>
           <div className="flex items-center mt-[25px] gap-2">
-            {/* <Button className='rounded-md py-3 px-2 border bg-green-300'  color="info">
-              Info
-            </Button>
-            <Button className='rounded-md py-3 px-2 border bg-green-500'  color="success">
-              Success
-            </Button>
-            <Button className='rounded-md py-3 px-2 border bg-yellow-500' color="warning">
-              Warning
-            </Button>
-            <Button className='rounded-md py-3 px-2 border bg-red-500' color="error">
-              Error
-            </Button> */}
+            <button className="btn btn-primary btn-outline border py-3 px-2 rounded-full hover:bg-orange-500">daisy button</button>
           </div>
-        </div>
+      </div>
     </>
   )
 }
